5da19a78ab7a3e8219d1629cf5539a3d67d3444e
Eugene Ivanov <wied11@yandex.ru>
2022-05-30 15:01:36 +0500

    realize game maket

--- app/src/main/java/com/tg/ttrs/GameActivity.kt
+++ app/src/main/java/com/tg/ttrs/GameActivity.kt
@@ -2,10 +2,33 @@ package com.tg.ttrs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.tg.ttrs.storage.AppPreferences
 
 class GameActivity : AppCompatActivity() {
+
+    var tvHighScore: TextView? = null
+    var tvCurrentScore: TextView? = null
+    var appPreferences: AppPreferences? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+        appPreferences = AppPreferences(this)
+
+        val btnRestart = findViewById<Button>(R.id.btn_restart)
+        tvHighScore = findViewById(R.id.tv_high_score)
+        tvCurrentScore = findViewById(R.id.tv_current_score)
+        updateHighScore()
+        updateCurrentScore()
+    }
+
+    private fun updateCurrentScore() {
+        tvCurrentScore?.text = "0"
+    }
+
+    private fun updateHighScore() {
+        tvHighScore?.text = "${appPreferences?.getHighScore()}"
     }
 }
